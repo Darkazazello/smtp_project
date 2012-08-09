28cdeda8b7b98eddf342e5b017695954cc4084a4
Aza <darkazazello@gmail.com>
2012-08-09 23:21:08 +0300

    Clear all

--- include/smtp.hrl
+++
@@ -1,8 +0,0 @@
--define(MAX_RECORDS, 10000).
--record(smtp_state,{
-     user  = undefined,
-     email = undefined,
-     host  = undefined,
-     from  = undefined,
-     rcpt  = [],
-     mail  = undefined}).
--- src/tcp_listener.erl
+++
@@ -1,8 +0,0 @@
-start_parallel_server() ->
-{ok, Listen} = gen_tcp:listen(...),
-spawn(fun() -> par_connect(Listen) end).
-par_connect(Listen) ->
-{ok, Socket} = gen_tcp:accept(Listen),
-spawn(fun() -> par_connect(Listen) end),
-loop(Socket).
-loop(..) ->
