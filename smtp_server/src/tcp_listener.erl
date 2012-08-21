a6bf725e02fddb2bb65704d6e145c291ddf09635
unknown <s.sheluhin@sheluhins.itransition.corp>
2012-08-21 15:16:04 +0300

    Add events to tcp_listener

--- smtp_server/src/tcp_listener.erl
+++ smtp_server/src/tcp_listener.erl
@@ -18,7 +18,7 @@ connect(Listen) ->
     error_logger:info_msg("Server received connection from~p~n", [Ip]),
     case ets:lookup(fsm, {client, Ip}) of
         [] -> 
-            {ok,Pid} = smtp_fsm:start(Socket),
+            {ok,Pid} = smtp_fsm:start(self()),
             ets:insert(fsm, {{client,Ip}, {fsm, Pid}}),
 	    error_logger:info_msg("Save client's socket~n"),
             loop(Socket, Pid);
@@ -30,16 +30,28 @@ connect(Listen) ->
        
     end.
     
-loop(Socket, Pid) ->
+loop(Socket, FsmPid) ->
     receive
         {tcp, Socket, Bin} ->
 	        error_logger:info_report("Receive request~p", binary:bin_to_list(Bin)),
-                gen_fsm:sync_send_event(Pid, binary:bin_to_list(Bin)),            
-            loop(Socket, Pid);
+                gen_fsm:sync_send_event(FsmPid, binary:bin_to_list(Bin)),            
+            loop(Socket, FsmPid);
         {tcp_close} ->
             gen_tcp:close(Socket),  
-            error_logger:info_report("Server closed socket")
+            error_logger:info_report("Client closed socket");
+        {send, Data} ->
+            gen_tcp:send(Socket, binary:list_to_bin(Data)),
+            inet:setopts(Socket, [{active, once}]),
+            loop(Socket, FsmPid);
+        {close} ->
+            close_socket(Socket),
+            error_logger:info_msg("Nice work~n")
         after ?MAX_TIMEOUT ->
 	    error_logger:info_report("Server closed socket"),
             gen_tcp:close(Socket)   
     end.
+
+close_socket(Socket) ->
+    gen_tcp:close(socket),
+    {ok, {Ip, Port}} = inet:peername(Socket),
+    ets:delete(fsm, {client, Ip}).
