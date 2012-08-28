b06950d31975a1307c4052257da2bf6d80653c9b
Aza <darkazazello@gmail.com>
2012-08-28 07:36:44 +0300

    Change application config

--- smtp_server/src/tcp_listener.erl
+++ smtp_server/src/tcp_listener.erl
@@ -1,9 +1,8 @@
-<<<<<<< HEAD
 -module(tcp_listener).
 
 -behaviour(gen_server).
 
--export([start_link/1, start/1]).
+-export([start_link/1, start/1, terminate_connections/1]).
 
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2,
          terminate/2, code_change/3, loop/2]).
@@ -26,14 +25,15 @@ start(Port) ->
 init([Port]) ->
   {ok, Listen} = gen_tcp:listen(Port, ?OPTS),
   ets:new(connections, [set,named_table]),
-  spawn(fun() -> connect(Listen, Port) end), 	
+  Pid = spawn(fun() -> connect(Listen, Port) end), 	
+  ets:insert(connections, {0,Pid}),  
   {ok, #state{}}.
 handle_call(_Request, _From, State) ->
   Reply = ok,
   {reply, Reply, State}.
 
 handle_cast({socket, Socket}, State) ->
-	{ok, {Address, _Port}} = inet:peername(Socket),
+    {ok, {Address, _Port}} = inet:peername(Socket),
     Ip = inet_parse:ntoa(Address),
     error_logger:info_msg("Server received connection from~p~n", [Ip]),
     case ets:lookup(fsm, {client, Ip}) of
@@ -75,27 +75,40 @@ handle_info(_Info, State) ->
   {noreply, State}.
 
 terminate(_Reason, _State) ->
+    terminate_connections(ets:tab2list(connections)),
     %%TODO terminate connections
     ok.
 
 code_change(_OldVsn, State, _Extra) ->
   {ok, State}.
+terminate_connections([]) ->
+    ok;
+terminate_connections([{Key,Pid}|B]) ->
+    Pid ! {'EXIT', "terminate", self()},
+    io:format("Key ~p~n", [Key]),
+    terminate_connections(B).
 
 connect(Listen, Port) ->
     case gen_tcp:accept(Listen) of
 	{ok,Socket} ->
 	    gen_server:cast(?SERVER, {socket, Socket}),
 	    connect(Listen, Port);
+	{'EXIT', Reason, Pid} ->
+	    error_logger:info_msg("Terminate connect");
 	_Any ->
-	    error_logger:info_msg("Can't open connection: ~p~n", [_Any]),
-	    {ok, Listen} = gen_tcp:listen(Port, ?OPTS),
-	    connect(Listen, Port)
+	    error_logger:info_msg("Can't open connection: ~p~n", [_Any,self()]),
+	    case gen_tcp:listen(Port, ?OPTS) of
+		{ok, Listen_} ->
+		    connect(Listen_, Port);
+		__Any ->
+		    error_logger:info_msg("Can't open connection: ~p~n", [__Any,self()])
+	    end
     end.
 
 loop(Socket, FsmPid) ->
     receive
         {tcp, Socket, Bin} ->
-	        error_logger:info_report("Receive request~p", binary:bin_to_list(Bin)),
+	    error_logger:info_report("Receive request~p", binary:bin_to_list(Bin)),
             gen_fsm:sync_send_event(FsmPid, binary:bin_to_list(Bin)),            
             loop(Socket, FsmPid);
         {tcp_close} ->
@@ -107,7 +120,9 @@ loop(Socket, FsmPid) ->
             loop(Socket, FsmPid);
         {close} ->
             close_socket(Socket),
-            error_logger:info_msg("Nice work~n")
+            error_logger:info_msg("Nice work~n");
+	{'EXIT', Reason, Pid} ->
+	    close_socket(Socket)
         after ?MAX_TIMEOUT ->
 	    error_logger:info_report("Server closed socket"),
             gen_tcp:close(Socket)   
@@ -117,97 +132,3 @@ close_socket(Socket) ->
     gen_tcp:close(socket),
     {ok, {Ip, _Port}} = inet:peername(Socket),
     ets:delete(fsm, {client, Ip}).
-=======
--module(tcp_listener).
-
--behaviour(gen_server).
-
--export([start_link/1]).
-
--export([init/1, handle_call/3, handle_cast/2, handle_info/2,
-         terminate/2, code_change/3]).
-
--include("../include/smtp.hrl").
-
--record(state, {}).
-
--define(SERVER, ?MODULE).
-
-
--define(OPTS, [binary, {packet, 4}, {reuseaddr, true}, 
-        {active, once}]).
-
-start_link(Port) ->
-  gen_server:start_link({local, ?SERVER}, ?MODULE, [Port], []).
-
-init([Port]) ->
-  {ok, Listen} = gen_tcp:listen(Port, [binary,{active, once}]),
-  spawn(fun() -> connect(Listen) end), 	
-  {ok, #state{}}.
-handle_call(_Request, _From, State) ->
-  Reply = ok,
-  {reply, Reply, State}.
-
-handle_cast({socket, Socket}, State) ->
-	{ok, {Address, _Port}} = inet:peername(Socket),
-    Ip = inet_parse:ntoa(Address),
-    error_logger:info_msg("Server received connection from~p~n", [Ip]),
-    case ets:lookup(fsm, {client, Ip}) of
-        [] -> 
-            error_logger:info_msg("Save client's socket~n"),
-            
-            %%{ok,FsmPid} = supervisor:start_child(fsm_sup, [Pid]),
-            FsmPid = 0,
-            Pid = spawn(fun() -> loop(Socket, FsmPid) end),
-            %ets:insert(fsm, {{client,Ip}, {fsm, FsmPid}})
-            gen_tcp:controlling_process(Socket, Pid);
-        [{{client,Ip},{fsm, FsmPid}}] ->
-	    	error_logger:info_msg("Received another connection~n"),
-            Pid = spawn(fun() -> loop(Socket, FsmPid) end),
-            gen_tcp:controlling_process(Socket, Pid);	
-	_Any ->
-	    error_logger:error("Something wrong with ets~p~n", [_Any])       
-    end,
-	{noreply, State}.
-
-
-handle_info(_Info, State) ->
-  {noreply, State}.
-
-terminate(_Reason, _State) ->
-  ok.
-
-code_change(_OldVsn, State, _Extra) ->
-  {ok, State}.
-
-connect(Listen) ->
-    {ok, Socket} = gen_tcp:accept(Listen),
-    gen_server:cast(?SERVER, {socket, Socket}),
-    connect(Listen).
-    
-loop(Socket, FsmPid) ->
-    receive
-        {tcp, Socket, Bin} ->
-	        error_logger:info_report("Receive request~p", binary:bin_to_list(Bin)),
-            %gen_fsm:sync_send_event(FsmPid, binary:bin_to_list(Bin)),            
-            loop(Socket, FsmPid);
-        {tcp_close} ->
-            gen_tcp:close(Socket),  
-            error_logger:info_report("Client closed socket");
-        {send, Data} ->
-            gen_tcp:send(Socket, binary:list_to_bin(Data)),
-            inet:setopts(Socket, [{active, once}]),
-            loop(Socket, FsmPid);
-        {close} ->
-            close_socket(Socket),
-            error_logger:info_msg("Nice work~n")
-        after ?MAX_TIMEOUT ->
-	    error_logger:info_report("Server closed socket"),
-            gen_tcp:close(Socket)   
-    end.
-
-close_socket(Socket) ->
-    gen_tcp:close(socket),
-    {ok, {Ip, _Port}} = inet:peername(Socket),
-    ets:delete(fsm, {client, Ip}).
->>>>>>> fa6f29b40e9121194e1b020d20976f26084fea4d
--- smtp_server/src/tcp_listener_sup.erl
+++ smtp_server/src/tcp_listener_sup.erl
@@ -1,6 +1,4 @@
-<<<<<<< HEAD
 -module(tcp_listener_sup).
-
 -behaviour(supervisor).
 
 %% API
@@ -28,36 +26,3 @@ init([Port]) ->
 	      Restart, Shutdown, Type, [tcp_listener]},
 
     {ok, {SupFlags, [AChild]}}.
-
-=======
--module(tcp_listener_sup).
-
--behaviour(supervisor).
-
-%% API
--export([start_link/1]).
-
-%% Supervisor callbacks
--export([init/1]).
-
--define(SERVER, ?MODULE).
-
-start_link(Port) ->
-    supervisor:start_link({local, tcp_listener_sup}, ?MODULE, [Port]).
-
-init([Port]) ->
-    RestartStrategy = one_for_one,
-    MaxRestarts = 1000,
-    MaxSecondsBetweenRestarts = 3600,
-    SupFlags = {RestartStrategy, MaxRestarts, MaxSecondsBetweenRestarts},
-
-    Restart = permanent ,
-    Shutdown = 2000,
-    Type = worker,
-
-    AChild = {tcp_listener, {tcp_listener, start_link, [Port]},
-	      Restart, Shutdown, Type, [tcp_listener]},
-
-    {ok, {SupFlags, [AChild]}}.
-
->>>>>>> fa6f29b40e9121194e1b020d20976f26084fea4d
